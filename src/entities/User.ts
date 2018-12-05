fc78b0ad694819a25e8261874e05e46e77dcc6e8
Seyoon <rlatpdbs@gmail.com>
2018-12-05 15:50:29 +0900

    1.14 User Entity part Two

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,44 +1,63 @@
 import { IsEmail } from 'class-validator'
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 class User extends BaseEntity {
     @PrimaryGeneratedColumn() id: number
 
-    @Column({type: "text", unique: true})
+    @Column({type: 'text', unique: true})
     @IsEmail()
     email: string
 
-    @Column({type: "boolean", default: false})
+    @Column({type: 'boolean', default: false})
     verifiedEmail: boolean
 
-    @Column({type: "text"})
+    @Column({type: 'text'})
     firstName: string
 
-    @Column({type: "text"})
+    @Column({type: 'text'})
     lastName: string
 
-    @Column({type: "int"})
+    @Column({type: 'int'})
     age: number
     
-    @Column({type: "text"})
+    @Column({type: 'text'})
     password: string
 
-    @Column({type: "text"})
+    @Column({type: 'text'})
     phoneNumber: string
 
-    @Column({type: "boolean", default: false})
+    @Column({type: 'boolean', default: false})
     verifiedPhoneNumber: boolean
 
-    @Column({type: "text"})
+    @Column({type: 'text'})
     profilePhoto: string
+
+    @Column({type: 'boolean', default: false})
+    isDriving: boolean
+
+    @Column({type: 'boolean', default: false})
+    isRiding: boolean
+
+    @Column({type: 'boolean', default: false})
+    isTaken: boolean
+
+    @Column({type: 'double precision', default: 0})
+    lastLng: number
+
+    @Column({type: 'double precision', default: 0})
+    lastLat: number
+
+    @Column({type: 'double precision', default: 0})
+    lastOrientation: number
+
+    get fullName() : string {
+        return `${this.firstName} ${this.lastName}`
+    }
 
     @CreateDateColumn() createdAt: string
 
     @UpdateDateColumn() updatedAt: string
-
-    @Column({type: "text"})
-    fullName: string
 }
 
 export default User
