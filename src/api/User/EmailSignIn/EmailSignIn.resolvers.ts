cb3d260adebcd2209edfc7deb78ce240fa76aabe
Seyoon <rlatpdbs@gmail.com>
2018-12-09 16:33:24 +0900

    1.33 EmailSignIn Resolver part Two

--- src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -1,32 +1,48 @@
-import { Resolvers } from "src/types/resolvers";
-import { EmailSignInMutationArgs, EmailSignInResponse } from "src/types/graph";
 import User from "../../../entities/User";
+import {
+  EmailSignInMutationArgs,
+  EmailSignInResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
 
 const resolvers: Resolvers = {
-    Mutation: {
-        EmailSignIn : async (
-            _,
-            args: EmailSignInMutationArgs
-        ) : Promise<EmailSignInResponse> => {
-            const { email } = args
-            try {
-                const user = await User.findOne({ email })
-                if(!user) {
-                    return {
-                        ok: false,
-                        error: 'No User Found with that email',
-                        token: null
-                    }
-                }
-                
-            } catch(error) {
-                return {
-                    ok: false,
-                    error: error.message,
-                    token: null
-                }
-            }
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email, password } = args;
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: "No user found with that email",
+            token: null
+          };
         }
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: "Coming soon"
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Wrong password",
+            token: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
     }
-}
-export default resolvers
+  }
+};
+export default resolvers;
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -82,7 +82,7 @@ class User extends BaseEntity {
         return `${this.firstName} ${this.lastName}`
     }
 
-    public comparePassword(password: string) : Promise<boolean> {
+    public comparePassword(password: any) : Promise<boolean> {
         return bcrypt.compare(password, this.password)
     }
 
